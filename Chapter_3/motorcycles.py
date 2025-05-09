5911c98289e03aab9f3fa719c71a033ef29332ad
Md Ishtiak Ahmed <ishtiakrongon@outlook.com>
2025-05-09 23:45:16 +0600

    updated motorcycles.py

--- Chapter_3/motorcycles.py
+++ Chapter_3/motorcycles.py
@@ -20,3 +20,17 @@ print("New motorcycle brand list:- ")
 for motor in new_motorcycles:
     print(f"\t{motor.title()}")
 
+# Inserting elements into the list by addressing the position of a list
+new_motorcycles.insert(0, 'rtr')
+print(new_motorcycles)
+
+# Removing an item from the list by using del Statement
+del new_motorcycles[0]
+print(new_motorcycles)
+
+# Removing an item using pop() Method
+popped_motorcycle: str = new_motorcycles.pop()
+print(f"The last motorcycle I owned was {popped_motorcycle.title()}.")
+
+
+
