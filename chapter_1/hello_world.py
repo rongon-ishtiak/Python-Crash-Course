0faba5a514fc30efedb1ca03d1ba392e7e8a9fcc
Md Ishtiak Ahmed <ishtiakrongon@outlook.com>
2025-05-09 15:02:19 +0600

    Updated chapter 1 hello_world.py file

--- chapter_1/hello_world.py
+++ chapter_1/hello_world.py
@@ -1,1 +1,20 @@
 print("Hello Python World")
+
+# VARIABLES
+message = "Again Started!"
+print(message)
+
+# CHANGING CASE IN A STRING WITH METHODS
+name = "ishtiak ahmed"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print(name.strip())
+print(name.capitalize())
+
+# USING VARIABLES IN STRINGS
+first_name = "ishtiak"
+last_name = "ahmed"
+full_name = f"{first_name} {last_name}"
+
+print(full_name.title())
